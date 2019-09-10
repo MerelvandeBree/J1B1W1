97758b260ccb6c68ffb6eacfc4230496231bc9e7
Merel <99059127@mydavinci.nl>
2019-09-10 09:56:14 +0200

    Var toegevoegd

---
+++ b1w2l1/b1w2l1.js
@@ -0,0 +1,10 @@
+
+window.alert("Je moet 3 vragen beantwoorden")
+
+var name = prompt("Wat is je naam", " ")
+var surname = prompt("Wat is je achternaam", " ")
+var age = prompt("Hoe oud ben je", " ")
+
+document.write('<br/>' + "Naam : " + prompt("Wat is je naam?") );
+document.write('<br/>' + "Achternaam : " + prompt("Wat is je achternaam?") )
+document.write('<br/>' + "Leeftijd : " + prompt("Hoe oud ben je?") )
